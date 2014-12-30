54ae7bbfe9987ab5e0b51bd6ebdb881d5555daa2
Andreas Schlicker <andi-sw@web.de>
2014-12-30 12:54:01 +0100

    Contains all functions necessary for page 1
    page1DataFrame(): filtering of results that are shown in the plots
    plotHeatmapPage1(): view 1
    plotCategoryOverview(): view 2

--- plotCategoryOverview.r
+++ plotCategoryOverview.r
@@ -1,6 +1,12 @@
-plotCategoryOverview = function(results, score, scoreCutoff, cancerType) {
+##' Filtering of result frame according to user criteria
+##' @param results data.frame with all results
+##' @param scoreCutoff threshold that was selected by the user
+##' @param cancerType cancer type selected by the user
+##' @return a subset of the data.frame that fits the user's selection
+##' @author Andreas Schlicker
+page1DataFrame = function(results, scoreCutoff, cancerType) {
 	# Filter the genes according to user's criteria
-	genes = as.character(subset(results, cancer == cancerType & score.type == "combined" & score >= scoreCutoff)$gene)
+	genes = as.character(subset(results, cancer == cancerType & score.type == "combined" & score >= as.integer(scoreCutoff))$gene)
 	
 	# Sort the genes according to highest sum across all cancer types
 	# Get the subset with the selected genes and drop unused levels
@@ -12,8 +18,34 @@ plotCategoryOverview = function(results, score, scoreCutoff, cancerType) {
 	# Get the data.frame for plotting
 	result.df = subset(results, gene %in% genes)
 	result.df$gene = factor(result.df$gene, levels=gene.order)
-	result.df$score.type = factor(result.df$score.type, levels=c("CNA", "Expr", "Meth", "Mut", "shRNA", "combined"))
 	result.df$cancer = factor(result.df$cancer, levels=sort(unique(as.character(result.df$cancer))))
+	
+	result.df
+}
+
+##' Get the heatmap for view 1 of page 1.
+##' @params results a subsetted data.frame as returned by page1DataFrame()
+##' @params colorLow "#034b87" if selected score was TS or combined, else "gray98"
+##' @params colorHigh "#880000" if selected score was OG or combined, else "gray98"
+##' @return the heatmap object
+##' @author Andreas Schlicker
+plotHeatmapPage1 = function(results, scoreType=c("combined.score", "ts.score", "og.score")) {
+	result.df = results
+	colorLow = list(combined.score="#034b87", ts.score="#034b87", og.score="gray98") 
+	colorMid = list(combined.score="gray98"),
+	colorHigh = list(combined.score="#880000", ts.score="gray98", og.score="#880000")
+
+	getHeatmap(dataFrame=result.df, yaxis.theme=theme(axis.text.y=element_blank()), 
+	   	   color.low=colorLow[[scoreType]], color.mid=colorMid[[scoreType]], color.high=colorHigh[[scoreType]])
+}
+
+##' Plots view 2 of page 1
+##' @param results a subsetted data.frame as returned by page1DataFrame()
+##' @return the ggplot2 object with the plot for view 2 of page 1
+##' @author Andreas Schlicker
+plotCategoryOverview = function(results) {
+	result.df = results
+	result.df$score.type = factor(result.df$score.type, levels=c("CNA", "Expr", "Meth", "Mut", "shRNA", "combined"))
 	
 	# Overwrite the score column with the score type to make it categorical
 	# Combined scores are not plotted later
