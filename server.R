b550711596ca026bd4bd50b02a932e79869bcaf4
Andreas Schlicker <a.schlicker@nki.nl>
2015-01-06 23:01:01 +0100

    Changed some options and chromosome names

--- server.R
+++ server.R
@@ -90,8 +90,8 @@ shinyServer(function(input, output, session) {
   ################################################################################### 
   starterWidgets <- load("www/starterWidgets.RData")
   genes <- apply(geness, 1, function(r) r)
-  chrms <- c("chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11","chr12",
-             "chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20","chr21","chr22","chrX","chrY")
+  chrms <- c("1","2","3","4","5","6","7","8","9","10","11","12",
+             "13","14","15","16","17","18","19","20","21","22","23","24")
   ## this will return the genes from the selected pathway in comp4
   ## currently set to false 4 values
   demoGenesByPathway <- c(genes[1],genes[100],genes[1000],genes[2000])
@@ -372,8 +372,8 @@ shinyServer(function(input, output, session) {
   ## score type selector comp3
   output$scoreSelectInputC3 <- renderUI({
     selectInput("selectScoreTypeC3", label = "Score type", 
-                choices = list("Choice 1" = 1, "Choice 2" = 2,
-                               "Choice 3" = 3), selected = 1)
+                choices = list("Oncogene score" = "OG", "Tumor suppressor gene score" = "TS",
+                               "Combined score" = "CO"), selected = "OG")
   })
   ## chr selector comp3
   output$chrSelector <- renderUI({
